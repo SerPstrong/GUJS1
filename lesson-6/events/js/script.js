ac2f8905736af415b7815250a920aeb2dda6bdfa
serpstrong <ommuts@gmail.com>
2020-12-10 15:13:49 +0500

    l6

---
+++ lesson-6/events/js/script.js
@@ -0,0 +1,13 @@
+
+function myProcessor(){
+    console.log("Загрузка завершена");
+}
+
+window.onload = myProcessor;
+
+//-------------------------------------
+var elScor = document.createElement("div");
+objScor.append(elScor);
+elScor.innerHTML = "Ваш счет -" + score;
+document.querySelector(".button").append(objScor);
+elScor.innerHTML = "Ваш счет -" + score;
